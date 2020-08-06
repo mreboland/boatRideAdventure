21f46f276d89e72ede3728dda154ac8c068d44c4
Edward Boland <mr.eboland@gmail.com>
2020-08-06 14:41:40 -0400

    css work, moved asset folder, minor html work

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <header>
+          <div className="wrapper header">
           <ul>
             <li><a href="">Home</a></li>
             <li><a href="">About</a></li>
@@ -16,9 +17,11 @@ class App extends Component {
           <div>
             <h1>BOATRIDE ADVENTURES</h1>
           </div>
+          </div>
         </header>
 
         <section>
+          <div className="wrapper">
           <ul>
             <li>
               <div>
@@ -53,42 +56,48 @@ class App extends Component {
               </div>
             </li>
           </ul>
-        </section>
-
-        <section>
-          <div>
-
-            <div>
-              <img src="" alt=""/>
-            </div>
-            <div>
-              <h2>OUR PROGRAMS</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque provident maxime quis aliquid voluptatibus culpa nostrum? Dolor eius excepturi incidunt. Deleniti autem similique ratione! Deserunt?</p>
-              <button>READ MORE</button>
-            </div>
-
-          </div>
-
-          <div>
-            
-            <div>
-              <h2>NEW LOCATIONS</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque provident maxime quis aliquid voluptatibus culpa nostrum? Dolor eius excepturi incidunt. Deleniti autem similique ratione! Deserunt?</p>
-              <button>READ MORE</button>
-            </div>
-            <div>
-              <img src="" alt="" />
-            </div>
-
           </div>
         </section>
 
         <section>
+          <div className="wrapper">
+            <div>
 
-          <div>
-            <h2>EXTRA VACATION DAYS?</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum perferendis qui assumenda repudiandae pariatur nisi! Tempora eius dolorem perferendis quis labore nisi asperiores laudantium quas culpa, facere molestiae amet voluptates fugiat nostrum molestias sit itaque. Sapiente consequuntur nisi necessitatibus quam, ullam modi facilis facere quod. Quam similique quibusdam perferendis aspernatur.</p>
-            <button>FIND OUT MORE</button>
+              <div>
+                <img src="" alt=""/>
+              </div>
+              <div>
+                <h2>OUR PROGRAMS</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque provident maxime quis aliquid voluptatibus culpa nostrum? Dolor eius excepturi incidunt. Deleniti autem similique ratione! Deserunt?</p>
+                <button>READ MORE</button>
+              </div>
+
+            </div>
+
+            <div>
+              
+              <div>
+                <h2>NEW LOCATIONS</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque provident maxime quis aliquid voluptatibus culpa nostrum? Dolor eius excepturi incidunt. Deleniti autem similique ratione! Deserunt?</p>
+                <button>READ MORE</button>
+              </div>
+              <div>
+                <img src="" alt="" />
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+        <section>
+
+          <div className="wrapper">
+            <div>
+              <h2>EXTRA VACATION DAYS?</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum perferendis qui assumenda repudiandae pariatur nisi! Tempora eius dolorem perferendis quis labore nisi asperiores laudantium quas culpa, facere molestiae amet voluptates fugiat nostrum molestias sit itaque. Sapiente consequuntur nisi necessitatibus quam, ullam modi facilis facere quod. Quam similique quibusdam perferendis aspernatur.</p>
+              <button>FIND OUT MORE</button>
+            </div>
           </div>
 
         </section>
@@ -99,27 +108,32 @@ class App extends Component {
 
         <section>
 
-          <div>
-            <img src="" alt=""/>
+          <div className="wrapper">
+            <div>
+              <img src="" alt=""/>
+            </div>
+
+            <div>
+              <h2>WIN A TRIP OF A LIFETIME</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tempore laudantium dolorem! Eligendi natus perferendis similique harum, corrupti possimus quod nesciunt veritatis sequi aut blanditiis accusamus modi voluptatibus praesentium est laborum rem, repellendus magnam? Ab temporibus, consectetur delectus officiis atque ut, amet magnam optio id necessitatibus ex placeat officia! Quidem?</p>
+              <button>FIND OUT MORE</button>
+            </div>
           </div>
 
-          <div>
-            <h2>WIN A TRIP OF A LIFETIME</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum tempore laudantium dolorem! Eligendi natus perferendis similique harum, corrupti possimus quod nesciunt veritatis sequi aut blanditiis accusamus modi voluptatibus praesentium est laborum rem, repellendus magnam? Ab temporibus, consectetur delectus officiis atque ut, amet magnam optio id necessitatibus ex placeat officia! Quidem?</p>
-            <button>FIND OUT MORE</button>
-          </div>
 
         </section>
 
         <section>
-
-          <div>
-            <ul>
-              <li><img src="" alt=""/></li>
-              <li><img src="" alt=""/></li>
-              <li><img src="" alt=""/></li>
-            </ul>
+          <div className="wrapper">
+            <div>
+              <ul>
+                <li><img src="" alt=""/></li>
+                <li><img src="" alt=""/></li>
+                <li><img src="" alt=""/></li>
+              </ul>
+            </div>
           </div>
+
 
         </section>
 
